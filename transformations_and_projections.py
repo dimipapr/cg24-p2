d65ab60c780f6cc22b3a8229a6c999d622e6108a
Dimitris Papadimitriou <jim_pap@live.com>
2024-04-25 22:56:38 +0300

    added Transform class skeleton

---
+++ transformations_and_projections.py
@@ -0,0 +1,48 @@
+import numpy as np
+
+class Transform:
+    """
+    TODO some real docstring
+    A class for preforming affine transformations
+    
+    Attributes:
+        mat(numpy.ndarray): A 4 by 4 numpy array representing an
+            affine transformation matrix
+    """
+    
+    def __init__(self):
+        """
+        Initialize a transform object
+        """
+        self.mat = np.identity(4)
+
+    def rotate(self, theta: float, u: np.ndarray) -> None:
+        """
+        Rotate the transformation matrix.
+
+        Args:
+            theta(float): Angle of rotation in (TODO radians or degrees?)
+            u(numpy.ndarray): Axis of rotation
+        """
+        
+        pass
+
+    def translate(self, t: np.ndarray) -> None:
+        """
+        Translate the transformation matrix.
+
+        Args:
+            t(np.ndarray): Translation vector
+        """
+        pass
+
+    def transform_pts(self, pts: np.ndarray) -> np.ndarray:
+        """
+        Transform the specified points according to transformation
+            matrix.
+        
+        Args:
+            pts(numpy.ndarray): Points (N) to apply the transformation on.
+                Matrix size = Nx3
+        """
+        pass
