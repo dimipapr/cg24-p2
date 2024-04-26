46a996ec1f4f2e83c8bcd553984397675e146a6b
Dimitris Papadimitriou <jim_pap@live.com>
2024-04-26 18:30:17 +0300

    added test for rotation by pi/4 around x axis

--- tests.py
+++ tests.py
@@ -12,6 +12,21 @@ class TestTransform(unittest.TestCase):
         obj = tp.Transform()
         self.assertTrue((obj.mat == np.identity(4)).all(),
                          "Transform matrix does not initialize to identity matrix.")
+    def test_rotate_x_pi_quarter(self):
+        tr = tp.Transform()
+        u = np.array([1,0,0])
+        theta = np.pi/4
+        tr.rotate(theta,u)
+        expected_result = np.array([
+            [1,0            , 0             ,0],
+            [0,np.cos(theta), -np.sin(theta),0],
+            [0,np.sin(theta), np.cos(theta) ,0],
+            [0,0            ,0              ,1]
+        ])
+        self.assertTrue(
+            (tr.mat == expected_result).all(),
+            "Rotation matrix for rot x by pi/4 is not as expected!",
+        )
 
 if __name__=="__main__":
     unittest.main()
