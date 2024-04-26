dffd9d33d4df57170de6def2883e7ee35b70fa5f
Dimitris Papadimitriou <jim_pap@live.com>
2024-04-26 18:19:24 +0300

    implemented Transformation.rotate()

--- transformations_and_projections.py
+++ transformations_and_projections.py
@@ -24,12 +24,23 @@ class Transform:
             theta(float): Angle of rotation in radians.
             u(numpy.ndarray): Axis of rotation
         """
-        
+        (ux,uy,uz) = u
         a = np.array([
-            
+            [ux*ux, ux*uy, ux*uz],
+            [uy*ux, uy*uy, uy*uz],
+            [uz*ux, uz*uy, uz*uz],
         ])
 
-        pass
+        b = np.array([
+            [0, -uz, uy],
+            [uz, 0, -ux],
+            [uy, ux, 0],
+        ])
+        R = (1-np.cos(theta))*a \
+            + np.cos(theta)*np.identity(3) \
+            +np.sin(theta)*b
+
+        self.mat[0:3,0:3] = R
 
     def translate(self, t: np.ndarray) -> None:
         """
